98488f61c4dc117698f6b399f2ea001b8f432d21
indrajxworkz <indraj.xworkz@gmail.com>
2025-04-21 14:09:38 +0530

    Now committed on 14-09

---
+++ src/main/java/com/mysticarts/newapp/exception/PhoneNumberNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mysticarts.newapp.exception;
+
+public class PhoneNumberNotFoundException extends RuntimeException {
+    public PhoneNumberNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/mysticarts/newapp/exception/PhonenumberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mysticarts.newapp.exception;
-
-public class PhonenumberNotFoundException extends RuntimeException {
-    public PhonenumberNotFoundException(String message) {
-        super(message);
-    }
-}
